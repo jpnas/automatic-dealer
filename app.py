545cc8c4eb3a96550c9c1179c6a92ed9f50b7061
João Nascimento <joao.nascimento@beegin.com.br>
2023-11-23 01:06:48 -0300

    working

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
 from games.models import CardGame
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/games', methods=['GET'])
 def get_games():
@@ -13,16 +14,17 @@ def get_games():
 @app.route('/games', methods=['POST'])
 def create_game():
     game_data = request.json
+    game_data['id'] = str(uuid.uuid4())
     CardGame.create(game_data)
-    return jsonify({"message": "Game created"}), 201
+    return jsonify({"message": "Game created", "id": game_data['id']}), 201
 
-@app.route('/games/<int:game_id>', methods=['POST'])
+@app.route('/games/<string:game_id>', methods=['PUT'])
 def update_game(game_id):
     game_data = request.json
     CardGame.update(game_id, game_data)
     return jsonify({"message": "Game updated"})
 
-@app.route('/games/<int:game_id>', methods=['DELETE'])
+@app.route('/games/<string:game_id>', methods=['DELETE'])
 def delete_game(game_id):
     CardGame.delete(game_id)
     return jsonify({"message": "Game deleted"})
--- db.py
+++ db.py
@@ -1,6 +1,18 @@
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pymongo import ssl_support
 
 def get_db():
-    client = MongoClient("mongodb://localhost:27017/")
+    uri = "mongodb+srv://jpnas:iHBXPcSA5a3Rxph9@cluster0.482qyxh.mongodb.net/?retryWrites=true&w=majority"
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'), tls=ssl_support.HAVE_SSL, tlsAllowInvalidCertificates=True)
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
     db = client["card_games"]
     return db
