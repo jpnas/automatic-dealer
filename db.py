f611eefad8164dea66971209f480d048c1244fac
João Nascimento <joao.nascimento@beegin.com.br>
2023-11-29 16:26:27 -0300

    fix

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+import ssl
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from pymongo import ssl_support
@@ -6,7 +7,10 @@ def get_db():
     uri = "mongodb+srv://jpnas:iHBXPcSA5a3Rxph9@cluster0.482qyxh.mongodb.net/?retryWrites=true&w=majority"
 
     # Create a new client and connect to the server
-    client = MongoClient(uri, server_api=ServerApi('1'), tls=ssl_support.HAVE_SSL, tlsAllowInvalidCertificates=True)
+    client = MongoClient(uri,
+                     server_api=ServerApi('1'),
+                     tls=ssl_support.HAVE_SSL,
+                     tlsAllowInvalidCertificates=True)
     # Send a ping to confirm a successful connection
     try:
         client.admin.command('ping')
